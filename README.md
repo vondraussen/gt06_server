311988aae859bc039e816bee444173c02c4e3148
Andy <hempel.andy@gmail.com>
2019-06-08 02:43:54 +0200

    Add MQTTS config

--- README.md
+++ README.md
@@ -9,8 +9,15 @@ So it acts as a server for GT06 trackers and a gateway to MQTT.
 ## Configuration
 The following environment variables are recognized. If not defined a default will be used.
 - GT06_SERVER_PORT=64459
-- MQTT_ROOT_TOPIC=gt06/
+- MQTT_ROOT_TOPIC=gt06
 - MQTT_BROKER_URL=localhost
 - MQTT_BROKER_PORT=1883
+- MQTT_BROKER_PROTO=mqtt
+- MQTT_BROKER_CA=/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem
 - MQTT_BROKER_USER=user
 - MQTT_BROKER_PASSWD=passwd
+
+## MQTT
+Messages received on the TCP port will be transmitted via MQTT *MQTT_ROOT_TOPIC/IMEI/pos*
+
+For example: *gt06/123456789012345/pos*
--- app.js
+++ app.js
@@ -2,18 +2,24 @@ require('dotenv').config();
 const Gt06 = require('./gt06');
 const Mqtt = require('mqtt');
 const net = require('net');
+const fs = require('fs');
 
 const serverPort = process.env.GT06_SERVER_PORT || 64459;
-const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06/';
+const rootTopic = process.env.MQTT_ROOT_TOPIC || 'gt06';
 const brokerUrl = process.env.MQTT_BROKER_URL || 'localhost';
 const brokerPort = process.env.MQTT_BROKER_PORT || 1883;
+const mqttProtocol = process.env.MQTT_BROKER_PROTO || 'mqtt';
 const brokerUser = process.env.MQTT_BROKER_USER || 'user';
 const brokerPasswd = process.env.MQTT_BROKER_PASSWD || 'passwd';
+const trustedCaPath = process.env.MQTT_BROKER_CA || '';
+const TRUSTED_CA = fs.readFileSync(trustedCaPath);
 
 var mqttClient = Mqtt.connect(
     {
         host: brokerUrl,
         port: brokerPort,
+        protocol: mqttProtocol,
+        ca: TRUSTED_CA,
         username: brokerUser,
         password: brokerPasswd
     });
@@ -38,7 +44,7 @@ var server = net.createServer((client) => {
         if (gt06.expectsResponse) {
             client.write(gt06.responseMsg);
         }
-        mqttClient.publish(rootTopic + gt06.imei + '/pos', JSON.stringify(gt06));
+        mqttClient.publish(rootTopic + '/' + gt06.imei + '/pos', JSON.stringify(gt06));
     });
 });
 
